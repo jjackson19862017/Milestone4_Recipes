ce7409f90c9a8bd61dd95b8c8e712f2bf5a23315
Stephen Jackson <jjackson19862017@gmail.com>
2020-02-04 03:12:59 +0000

    Setup

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for, request
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = '' # Insert Database Name
+app.config["MONGO_URI"] = '' # Insert Database URI from MongoDB
+
+mongo = PyMongo(app)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
